fce3fdc3aa47f5285ed078d24bb000b79a15b5cd
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-05-14 20:13:57 +0530

    wip

--- src/components/VideoListing.js
+++ src/components/VideoListing.js
@@ -1,3 +1,52 @@
 export const VideoListing = () => {
-    return <div>VideoListing</div>
+    return     <div className="videos-container">
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651992022/indian_cuisine_yh7kv7.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651992022/indian_cuisine_yh7kv7.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">ABC</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651992944/chinese_cuisine_tjtdlb.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651992944/chinese_cuisine_tjtdlb.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">DEF</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651993176/italian_cusine_emauxd.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651993176/italian_cusine_emauxd.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">IJK</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651992022/indian_cuisine_yh7kv7.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651992022/indian_cuisine_yh7kv7.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">ABC</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651992944/chinese_cuisine_tjtdlb.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651992944/chinese_cuisine_tjtdlb.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">DEF</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651993176/italian_cusine_emauxd.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651993176/italian_cusine_emauxd.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">IJK</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651993176/italian_cusine_emauxd.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651993176/italian_cusine_emauxd.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">IJK</div>
+    </div>
+    <div className="video-container">
+    <video autoPlay muted loop poster="https://res.cloudinary.com/duddwta8d/video/upload/v1651993176/italian_cusine_emauxd.jpg" alt="cuisine">
+         <source src="https://res.cloudinary.com/duddwta8d/video/upload/e_blur:100/v1651993176/italian_cusine_emauxd.mp4" type="video/mp4" />
+     </video>
+     <div className="small-heading">IJK</div>
+    </div>
+ </div>
 }
