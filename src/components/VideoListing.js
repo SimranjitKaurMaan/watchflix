01a7a0752a91efe4b358aa2b37b309360536e57b
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-05-29 19:33:46 +0530

    Added video listing page

--- src/components/VideoListing.js
+++ src/components/VideoListing.js
@@ -1,9 +1,20 @@
+import { SideNavBar } from "./customComponents/Navbar/SideNavBar";
 import { VideoCard } from "./customComponents/VideoCard";
 
 export const VideoListing = () => {
   return (
-    <div className="videos-container flex-row-wrap-center">
-      <VideoCard/>
+    <div className="videos-container">
+        <SideNavBar/>
+        <main className="flex-row-wrap-start-center">
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+        </main>
     </div>
   );
 };
