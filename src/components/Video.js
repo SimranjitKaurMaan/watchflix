c277cb84abd6bfbe1197d9f5409a51d34733c5e6
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-08-13 18:06:49 +0530

    Fixed up next

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,25 +1,29 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { fetchVideo } from "../utils/requestUtils/VideosListingRequestUtils";
 import { VideoCard } from "./customComponents/VideoCard";
 import { VideoPlayer } from "./customComponents/VideoPlayer";
 
 export const Video = () => {
-const { id } = useParams();
-const [video, setVideo] = useState(null);
-useEffect(() => {
+  const { id } = useParams();
+  const [video, setVideo] = useState(null);
+  useEffect(() => {
     (async () => {
-    const video = await fetchVideo(id);
-    setVideo(video);
+      const video = await fetchVideo(id);
+      setVideo(video);
     })();
-},[])
-  return <div className="video-container-wrapper">
-      <main>
-          {video && <VideoPlayer video={video}/>}
-      </main>
-      {/* <aside>
-          <h2 className="highlight-text sm-left-margin">Up next</h2>
-          <VideoCard video={video}/>
-      </aside> */}
-  </div>
+  }, []);
+  return (
+    <div className="video-container-wrapper">
+      <main>{video && <VideoPlayer video={video} />}</main>
+      <aside>
+        {video && (
+          <Fragment>
+            <h2 className="highlight-text sm-left-margin">Up next</h2>
+            <VideoCard video={video} />
+          </Fragment>
+        )}
+      </aside>
+    </div>
+  );
 };
